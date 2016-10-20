5ecd015d579edd257273ad3a8a8c02e0ad49d961
Nigel Burnett and Skye Atwood <nigel@>
2016-10-20 16:36:30 -0700

    rearrange and add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+var tracker = function(number1, number2, number3) {
+
+  if (number1 === number2 && number2 === number3) {
+  return "equal";
+}
+else if (number1 === number2 || number2 === number3 || number3 === number1) {
+  return "isos";
+
+}
+else if (number1 + number2 < number3 || number2 + number3 < number1 || number3 + number1 < number2) {
+  return "not";
+
+}
+else {
+  return "scalene";
+}
+}
+
+//Everything below is logic, everything above is business.
+
 $(document).ready(function() {
 
 $("form").submit(function(event) {
@@ -8,19 +28,11 @@ $("form").submit(function(event) {
 
      var number3 = parseInt($("#side3").val());
 
-       console.log(number3);
-     if (number1 === number2 && number2 === number3) {
-       $('#equal').show();
-     }
-     else if (number1 === number2 || number2 === number3 || number3 === number1) {
-       $('#isos').show();
-     }
-     else if (number1 + number2 > number3 || number2 + number3 > number1 || number3 + number1 > number2) {
-       $('#scalene').show();
-     }
-     else {
-       $('#not').show();
-     }
+     var result = tracker(number1, number2, number3);
+
+     $("#" + result).show();
+
+
   event.preventDefault();
 });
 
